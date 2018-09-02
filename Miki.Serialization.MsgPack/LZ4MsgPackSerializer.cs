9d362288d008e870f3f5bb336a5ef72a44db640f
Veld <velddev@gmail.com>
2018-09-02 17:53:32 +0200

    Add: MsgPack

---
+++ Miki.Serialization.MsgPack/LZ4MsgPackSerializer.cs
@@ -0,0 +1,20 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.Serialization.MsgPack
+{
+	public class LZ4MsgPackSerializer : ISerializer
+	{
+		public T Deserialize<T>(byte[] data)
+		{
+			return LZ4MessagePackSerializer.Deserialize<T>(data);
+		}
+
+		public byte[] Serialize<T>(T data)
+		{
+			return LZ4MessagePackSerializer.Serialize(data);
+		}
+	}
+}
---
+++ Miki.Serialization.MsgPack/MsgPackSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MessagePack;
+
+namespace Miki.Serialization.MsgPack
+{
+	public class MsgPackSerializer : ISerializer
+	{
+		public T Deserialize<T>(byte[] data)
+		{
+			return MessagePackSerializer.Deserialize<T>(data);
+		}
+
+		public byte[] Serialize<T>(T data)
+		{
+			return MessagePackSerializer.Serialize(data);
+		}
+	}
+}
