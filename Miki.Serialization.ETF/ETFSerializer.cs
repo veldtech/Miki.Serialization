3076531080238d82c9170939c4339639a93116c0
Veld <velddev@gmail.com>
2019-06-10 18:52:09 +0200

    Add: erlpack serializer project

---
+++ Miki.Serialization.ETF/ETFSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Miki.Serialization;
+
+namespace Miki.Serialization.ETF
+{
+    public class ETFSerializer : ISerializer
+    {
+        public T Deserialize<T>(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize<T>(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Miki.Serialization.ETF/Reader/ETFReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Miki.Serialization.ETF.Reader
+{
+    public class ETFReader : IDisposable
+    {
+        private MemoryStream _stream;
+
+        private bool _isDisposed;
+        private int _offset;
+
+        public ETFReader(byte[] packet)
+        {
+        }  
+
+        private byte[] ReadBytes(int num)
+        {
+            byte[] val = new byte[num];
+            var bytesRead = _stream.Read(val, _offset, num);
+            if(bytesRead != num)
+            {
+                throw new EndOfStreamException();
+            }
+            _offset += num;
+            return val;
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+            _isDisposed = true;
+        }
+    }
+}
